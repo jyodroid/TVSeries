2ed1056d3593bf82bb5bc0db799e0533624379be
John Tangarife <jyodroid@gmail.com>
2022-05-16 11:51:12 -0500

    Fix contrast problems on dark theme

--- app/src/main/java/com/jyodroid/tvseries/utils/DateTimeFormatter.kt
+++ app/src/main/java/com/jyodroid/tvseries/utils/DateTimeFormatter.kt
@@ -1,17 +1,30 @@
 package com.jyodroid.tvseries.utils
 
+import android.util.Log
 import java.text.SimpleDateFormat
 import java.util.*
 
 const val MAZE_DATE_FORMAT = "yyyy-MM-dd"
 const val COMPLETE_DATE_FORMAT = "EEE, MMM dd, yyyy"
 
+private const val TAG = "com.jyodroid.tvseries.utils.DateTimeFormatter"
+
 fun String.convertToDate(): Date? {
     val dateFormat = SimpleDateFormat(MAZE_DATE_FORMAT, Locale.getDefault())
-    return dateFormat.parse(this)
+    return try {
+        dateFormat.parse(this)
+    } catch (e: Exception) {
+        Log.e(TAG,"String To Date", e)
+        null
+    }
 }
 
 fun Date.convertToCompleteStringDate(): String? {
     val dateFormat = SimpleDateFormat(COMPLETE_DATE_FORMAT, Locale.getDefault())
-    return dateFormat.format(this)
+    return try {
+        dateFormat.format(this)
+    } catch (e: Exception) {
+        Log.e(TAG,"String To Date", e)
+        null
+    }
 }
